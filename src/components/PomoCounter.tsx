843ae705d6a5bc223194c9eaf101f306411dd8dd
folka2134 <folka2134@gmail.com>
2024-04-16 12:49:40 +0100

    updated styles

--- src/components/PomoCounter.tsx
+++ src/components/PomoCounter.tsx
@@ -2,7 +2,10 @@ const PomoCounter = ({ pomodoro, setPomodoro }: any) => {
   return (
     <div className="mt-5 flex flex-col items-center text-xl">
       <div>
-        Pomodoros: <span className="text-purple-700">{pomodoro}</span>
+        Pomodoros:{" "}
+        <span className="ml-1 rounded-full border-4 border-red-600 bg-red-500 px-4 py-2 text-2xl font-bold text-purple-700">
+          {pomodoro}
+        </span>
       </div>
       <button onClick={() => setPomodoro(0)}>Reset</button>
     </div>
